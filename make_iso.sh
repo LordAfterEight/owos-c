3056aa12b9a4ea8e6a43a7b51c3c7e68c722eae7
LordAfterEight <elias@stettmayer.de>
2026-01-11 00:36:16 +0100

    Initial commit

---
+++ make_iso.sh
@@ -0,0 +1,12 @@
+cp -v bin/owos-c iso_root/boot/
+cp -v limine.conf limine/limine-bios.sys limine/limine-bios-cd.bin \
+      limine/limine-uefi-cd.bin iso_root/boot/limine/
+# Create the bootable ISO.
+xorriso -as mkisofs -R -r -J -b boot/limine/limine-bios-cd.bin \
+        -no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus \
+        -apm-block-size 2048 --efi-boot boot/limine/limine-uefi-cd.bin \
+        -efi-boot-part --efi-boot-image --protective-msdos-label \
+        iso_root -o owos-c.iso
+
+# Install Limine stage 1 and 2 for legacy BIOS boot.
+./limine/limine bios-install owos-c.iso
---
+++ run.sh
@@ -0,0 +1,3 @@
+make
+./make_iso.sh
+qemu-system-x86_64 -m 2G -cdrom owos-c.iso
